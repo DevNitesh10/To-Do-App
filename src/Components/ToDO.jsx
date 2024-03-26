3170f5c669f8989cc312b6eb91fdc7bb79e0347c
DevNitesh10 <princenitesh325@gmail.com>
2024-03-26 21:48:35 +0545

    ...

---
+++ src/Components/ToDO.jsx
@@ -0,0 +1,9 @@
+import './CSS/Todo.css';
+
+const ToDO = () => {
+  return (
+    <div>ToDO</div>
+  )
+}
+
+export default ToDO
---
+++ src/Components/ToDoItems.jsx
@@ -0,0 +1,9 @@
+import './CSS/Todoitems.css';
+
+const ToDoItems = () => {
+  return (
+    <div>ToDoItems</div>
+  )
+}
+
+export default ToDoItems
